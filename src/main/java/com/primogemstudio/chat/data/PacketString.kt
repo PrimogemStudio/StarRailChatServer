edef59e7fbe6bfd91091aec37edce01f25b8d434
Jack253-png <guoxiuchen20170402@163.com>
2024-10-09 19:48:00 +0800

    Update

--- src/main/java/com/primogemstudio/chat/data/PacketString.kt
+++ src/main/java/com/primogemstudio/chat/data/PacketString.kt
@@ -11,18 +11,10 @@ class PacketString private constructor() : PacketDataType<String> {
     override fun getSize(): Int = -1
     override fun getName(): String = "String"
     override fun deserialize(raw: DataInput): String {
-        val size = PacketInt.INSTANCE.deserialize(raw)
-        val chars = CharArray(size)
-        for (i in 0 ..< size) {
-            chars[i] = raw.readChar()
-        }
-
-        return String(chars)
+        return raw.readUTF()
     }
 
     override fun serialize(data: Any, out: DataOutput) {
-        val d = (data as String).toCharArray()
-        PacketInt.INSTANCE.serialize(d.size, out)
-        d.forEach { out.writeChar(it.code) }
+        out.writeUTF(data as String)
     }
 }
